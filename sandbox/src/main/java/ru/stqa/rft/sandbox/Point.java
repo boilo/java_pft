f7f0cd3157285f22e6d601d99c485208421ff808
Alexander Boilo <alyaksandr@protonmail.ch>
2016-02-22 00:33:20 +0300

    Objects, methods added

---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
@@ -0,0 +1,18 @@
+package ru.stqa.rft.sandbox;
+
+/**
+ * Created by User on 21.02.2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+
+  public static double distance(Point p1, Point p2) {
+
+  }
+
+}
+
+
---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Square.java
@@ -0,0 +1,19 @@
+package ru.stqa.rft.sandbox;
+
+/**
+ * Created by User on 21.02.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public  double area() {
+    return this.l * this.l;
+  }
+
+
+}

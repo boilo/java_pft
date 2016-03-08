e463c008df9841c3243d3b9622811b400e81fd16
Alexander Boilo <alyaksandr@protonmail.ch>
2016-03-08 15:42:33 +0300

    Init for Chrome, Edge added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,12 +14,17 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
@@ -27,7 +36,13 @@ public class ApplicationManager {
   }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.EDGE)) {
+      wd = new EdgeDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     contactHelper = new ContactHelper(wd);
---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Equality.java
@@ -0,0 +1,15 @@
+package ru.stqa.rft.sandbox;
+
+/**
+ * Created by User on 08.03.2016.
+ */
+public class Equality {
+
+  public static void main (String[] args) {
+    String s1 = "firefox";
+    String s2 = new String(s1);
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}
